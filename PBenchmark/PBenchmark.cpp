1344826b74c4ee4c8fad755aa5bb0421caf27c52
Peppooo <pietrofacchini46@gmail.com>
2022-10-23 21:49:50 +0200

    Now the program is correct and have a sense

--- PBenchmark/PBenchmark.cpp
+++ PBenchmark/PBenchmark.cpp
@@ -9,19 +9,25 @@
 #include <vector>
 #include <chrono>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
-bool check_prime_number(int n) {
-    int i, m = 0, flag = 0;
-    m = n / 2;
-    for (i = 2; i <= m; i++)
-    {
-        if (n % i == 0)
-        {
-            flag = 1;
-            break;
-        }
+bool check_x_prime_numbers(int x) {
+    bool flag=false;
+    for (int num = 0; num < x; num++) {
+        //SetConsoleTitleA((string("checking ").append(std::to_string(num)).c_str()));
+        if (num <= 1) flag=false;
+        if (num <= 3)  flag=true;
+
+        double range = sqrt(num);
+        if (num % 2 == 0 || num % 3 == 0)
+            flag=false;
+
+
+        for (int i = 5; i <= range; i += 6)
+            if (num % i == 0 || num % (i + 2) == 0)
+                flag=false;
     }
     return flag;
 }
@@ -32,34 +38,30 @@ int main(int argc, char* argv[]) {
     LARGE_INTEGER end;
     vector<std::thread> operating_threads;
     double interval = 0.0;
-    int w = 0;
-    int n = 0;
-    cout << "Insert how many prime numbers to check: ";
-    cin >> w;
+    int prime_numbers_to_check = 0;
+    int threadstocreate = 0;
+    cout << "Insert how many prime numbers to check (per thread): ";
+    cin >> prime_numbers_to_check;
     cout << "How many threads: ";
-    cin >> n;
+    cin >> threadstocreate;
     QueryPerformanceFrequency(&frequency);
     QueryPerformanceCounter(&start);
-
-    for (int x = 0; x < n; x++) {
-        //cout << "calculating " << w/n << " prime numbers at thread " << x << endl;
-        for (int i = 0; i < w/n; i++) {
-            operating_threads.push_back(thread(check_prime_number, i));
-        }
+    // - start work -
+    for (int i = 0; i < threadstocreate; i++) {
+        operating_threads.push_back(thread(check_x_prime_numbers, prime_numbers_to_check));
     }
-
-
+    for (int i = 0; i < threadstocreate; i++) {
+        operating_threads[i].join();
+    }
+    // - end work -
     QueryPerformanceCounter(&end);
-    for (int y = 0; y < n; y++) {
-        operating_threads[y].join();
-    }
     interval = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
     cout << endl << "Time: " << interval << endl;
     ofstream benchmarks_results;
     benchmarks_results.open("benchmarks.log", ios_base::app);
     time_t now = time(0);
     string time = ctime(&now); time.erase(std::remove(time.begin(), time.end(), '\n'), time.cend());
-    benchmarks_results << "[" << time << "]: Threads (" << n << "); Prime Numbers (" << w << "); Time (" << interval << ");" << endl;
+    benchmarks_results << "[" << time << "]: Threads (" << threadstocreate << "); Prime Numbers (" << prime_numbers_to_check << "); Time (" << interval << ");" << endl;
     benchmarks_results.close();
     main(NULL, NULL);
 }
