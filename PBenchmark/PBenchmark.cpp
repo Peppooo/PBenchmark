4fc3aebf4e1678a8030c5479f636a3472ffdcf23
Peppooo <pietrofacchini46@gmail.com>
2022-10-23 22:51:21 +0200

    Added the possibility to do multiple test to compare and get best time

--- PBenchmark/PBenchmark.cpp
+++ PBenchmark/PBenchmark.cpp
@@ -32,36 +32,47 @@ bool check_x_prime_numbers(int x) {
     return flag;
 }
 
-int main(int argc, char* argv[]) {
+double execute_benchmark(int threadstocreate, int prime_numbers_to_check) {
     LARGE_INTEGER frequency;
     LARGE_INTEGER start;
     LARGE_INTEGER end;
-    vector<std::thread> operating_threads;
+    vector<thread> operating_threads;
     double interval = 0.0;
-    int prime_numbers_to_check = 0;
-    int threadstocreate = 0;
-    cout << "Insert how many prime numbers to check (per thread): ";
-    cin >> prime_numbers_to_check;
-    cout << "How many threads: ";
-    cin >> threadstocreate;
     QueryPerformanceFrequency(&frequency);
     QueryPerformanceCounter(&start);
-    // - start work -
     for (int i = 0; i < threadstocreate; i++) {
         operating_threads.push_back(thread(check_x_prime_numbers, prime_numbers_to_check));
     }
     for (int i = 0; i < threadstocreate; i++) {
         operating_threads[i].join();
     }
-    // - end work -
     QueryPerformanceCounter(&end);
-    interval = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
-    cout << endl << "Time: " << interval << endl;
+    return (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+}
+
+int main(int argc, char* argv[]) {
+    double lower_time = 0;
+    int prime_numbers_to_check;
+    int threadstocreate;
+    int tests;
+    vector<double> times;
+    cout << "Insert how many prime numbers to check (per thread): ";
+    cin >> prime_numbers_to_check;
+    cout << "How many threads: ";
+    cin >> threadstocreate;
+    cout << "How many tests: ";
+    cin >> tests;
+    // execute benchmarks
+    for (int i = 0; i < tests; i++) {
+        times.push_back(execute_benchmark(threadstocreate,prime_numbers_to_check));
+    }
+    lower_time = times[ranges::distance(times.begin(), ranges::min_element(times))];
+    cout << lower_time;
     ofstream benchmarks_results;
     benchmarks_results.open("benchmarks.log", ios_base::app);
     time_t now = time(0);
     string time = ctime(&now); time.erase(std::remove(time.begin(), time.end(), '\n'), time.cend());
-    benchmarks_results << "[" << time << "]: Threads (" << threadstocreate << "); Prime Numbers (" << prime_numbers_to_check << "); Time (" << interval << ");" << endl;
+    benchmarks_results << "[" << time << "]: threads (" << threadstocreate << "); prime Numbers (" << prime_numbers_to_check << "); best time (" << lower_time << ");" << endl;
     benchmarks_results.close();
     main(NULL, NULL);
 }
